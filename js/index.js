5821980e3a4c6b41fa072c7e9fafb706fc8db4b3
ingnicolaspj <68884962+ingnicolaspj@users.noreply.github.com>
2021-02-11 18:14:46 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+boteDeBasura = "Piedras";
+num1 = 100; //Numerico
+num2 = 50;
+
+resSum = num1 + num2;
+resRest = num1 - num2;
+resMult = num1 * num2;
+resDiv = num1 / num2;
+
+document.writeln(resDiv);
+
+boleano = false;// Boleanos
+
+
+//alert(boleano);
+//console.log(boteDeBasura);
+//document.writeln(boteDeBasura);
+//Strings numbers
